9028a69c8c5e0611f7deff33863746b2da743777
Vyacheslav Sininkin <72771001+slavastan@users.noreply.github.com>
2022-02-15 03:43:25 +0300

    Update urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,13 +1,7 @@
-from django.urls import include, path, re_path
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (
-    FavoriteCreateDestroy,
-    IngredientViewSet,
-    RecipeViewSet,
-    ShoppingListCreateDestroy,
-    TagViewSet,
-)
+from .views import IngredientViewSet, RecipeViewSet, TagViewSet
 
 router_v1 = DefaultRouter()
 
@@ -17,18 +11,5 @@ router_v1.register("tags", TagViewSet)
 
 
 urlpatterns = [
-    path(
-        "",
-        include(router_v1.urls),
-    ),
-    re_path(
-        r"recipes/(?P<recipe_id>\d+)/favorite/",
-        FavoriteCreateDestroy.as_view(),
-        name="favorite",
-    ),
-    re_path(
-        r"recipes/(?P<recipe_id>\d+)/shopping_cart/",
-        ShoppingListCreateDestroy.as_view(),
-        name="shopping-list",
-    ),
+    path("", include(router_v1.urls)),
 ]
